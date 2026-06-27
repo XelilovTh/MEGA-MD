2e6ab09a7533f668c99c4fc2dca1486a748ca184
Tehmaz Xelilov <tehmazxelilov7@gmail.com>
2026-06-27 18:01:29 +0400

    Create mycommand.js

---
+++ plugins/mycommand.js
@@ -0,0 +1,21 @@
+export default {
+    command: 'mycommand',
+    aliases: ['mc'],
+    category: 'utility',
+    description: 'Nəsə edir',
+    usage: '.mycommand <input>',
+
+    ownerOnly: false,
+    groupOnly: false,
+    adminOnly: false,
+    cooldown: 5,
+
+    async handler(sock, message, args, context = {}) {
+        const { chatId, channelInfo } = context;
+
+        await sock.sendMessage(chatId, {
+            text: `Yazdın: ${args.join(' ')}`,
+            ...channelInfo
+        }, { quoted: message });
+    }
+};
